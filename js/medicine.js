f509792dd38e9889204427f528d77b0a358d6ecb
Raf Theunis <raf.theunis@innovationstudio.be>
2013-09-22 10:33:11 +0200

    Changed the layout a bit.

--- js/medicine.js
+++ js/medicine.js
@@ -4,3 +4,24 @@ $(document).ready(function() {
         $('.toggle').slideToggle();
     });
 });
+
+function getAllMedicines() {
+    $.getJSON('http://medicinecabinets.apiary.io/v1/cabinets/1/medicines', function(data) {
+        for (var i = 0; i < data.medicines.length; i++) {
+            print = "<h3>" + data.medicines[i].name + " " + data.medicines[i].description + "<span  style='margin: inherit'><button type='button' class='btn btn-default btn-sm'> <span class='glyphicon glyphicon-star'></span></button>" +
+                "<button type='button' class='btn btn-default btn-sm'> <span class='glyphicon glyphicon-plus'></span></button></span>" + "</h3>" +
+                "<div id='" + data.medicines[i].name + "'>" +
+                "<p>" + data.medicines[i].usage + ", " + data.medicines[i].packaging.amount + " tabletten resterend" + "</p>" +
+                "<p>" + "Toegevoegd voor: " + data.medicines[i].familyMembers[0].name + "</p></div>";
+
+            $('#accordion').append(print);
+        }
+    })
+        .done(function(data) {
+        })
+        .complete(function(data) {
+            $("#accordion").accordion();
+        });
+}
+
+getAllMedicines();
